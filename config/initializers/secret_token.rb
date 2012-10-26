5822d79c6f374336c319dcdd918f87f7579377e7
mjurjovec <mjurjovec@gmail.com>
2012-10-26 10:11:41 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FollettShelf::Application.config.secret_token = 'dd01f6e24af5f6d50113aa6eb1cc462e807f9f3429cdb588c04561ab866cf337c42c632f665346602cdc83c0c8b1d61ea73f1b102c9d72d04384ab6a13b73fa4'
